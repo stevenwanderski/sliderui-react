7d4aa97db9d4049984803b41f7cb3a6a55b42820
Steven Wanderski <steven.wanderski@gmail.com>
2016-12-09 17:07:57 -0600

    Adds env console log

--- src/containers/home-container.jsx
+++ src/containers/home-container.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router';
 
 class HomeContainer extends React.Component {
   render() {
+    console.log(process.env.NODE_ENV);
     return <Link to="/temp/slider/new">Create a slider yo!</Link>;
   }
 }
