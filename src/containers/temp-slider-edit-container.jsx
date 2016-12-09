5aea71a7fdddd8dae15cd5ee0284276c5a076a64
Steven Wanderski <steven.wanderski@gmail.com>
2016-12-09 16:26:04 -0600

    Adds better tour language

--- src/containers/temp-slider-edit-container.jsx
+++ src/containers/temp-slider-edit-container.jsx
@@ -10,26 +10,26 @@ const steps = [
   {
     step: 1,
     selector: '.button--add-slide',
-    title: <div className='tour__title'>Start by adding slides</div>,
+    title: <div className='tour__title'>1. Add slides</div>,
     body: <div className='tour__body'>This is where you can upload images for the slider.</div>
   },
   {
     step: 2,
     selector: '.slider-layout__tab-link--preview',
-    title: <div className='tour__title'>Preview the slider</div>,
+    title: <div className='tour__title'>2. Preview the slider</div>,
     body: <div className='tour__body'>After adding slides, click here to see what the slider will look like. Feel free to switch back and forth as you make changes.</div>
   },
   {
     step: 3,
     selector: '.slider-settings__settings-header',
-    title: <div className='tour__title'>Tweak some slider settings</div>,
+    title: <div className='tour__title'>3. Tweak slider settings</div>,
     body: <div className='tour__body'>Use these settings to change how the slider behaves.</div>,
     position: 'bottom'
   },
   {
     step: 4,
     selector: '.button--get-code',
-    title: <div className='tour__title'>Finish up here</div>,
+    title: <div className='tour__title'>4. Finish up here</div>,
     body: <div className='tour__body'>Get the slider code and instructions on how to use it.</div>
   }
 ]
@@ -108,6 +108,7 @@ class TempSliderEditContainer extends React.Component {
             onNext={(step) => this.setState({ tourStep: step })}
             onBack={(step) => this.setState({ tourStep: step })}
             onCancel={() => this.setState({ isTourActive: false })}
+            hideClose={true}
             steps={steps}
             arrowColor='#8687c5'
         />
