a407c63c684632aad8d6a4de54097369ffedfed8
Steven Wanderski <steven.wanderski@gmail.com>
2016-12-09 17:11:25 -0600

    More console log

--- src/containers/home-container.jsx
+++ src/containers/home-container.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router';
 
 class HomeContainer extends React.Component {
   render() {
-    console.log(process.env.NODE_ENV);
+    console.log('NODE_ENV', process.env.NODE_ENV);
+    console.log('API_URL', process.env.API_URL);
     return <Link to="/temp/slider/new">Create a slider yo!</Link>;
   }
 }
