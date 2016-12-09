7d82f1ec0be62ce998f7872071294de7ed426b30
Steven Wanderski <steven.wanderski@gmail.com>
2016-12-09 17:26:28 -0600

    More prod config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -41,6 +41,14 @@ var config = {
       template: 'src/index-template.html',
       favicon: 'src/images/icon-star.png'
     }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': process.env.NODE_ENV || JSON.stringify('production'),
+        'API_URL': process.env.API_URL,
+        'BABEL_ENV': process.env.BABEL_ENV,
+        'SENTRY_KEY': process.env.SENTRY_KEY
+      }
+    }),
     new webpack.optimize.UglifyJsPlugin()
   ]
 };
